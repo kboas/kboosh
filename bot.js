869814f9ab39970dee333aa8b71b6006712fe6d0
7MMo0oD2016 <moatseemibrahim@gmail.com>
2018-12-06 17:00:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ client.on('message', message => {
     var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
 if (message.content.startsWith(prefix + 's')) {
+                                       message.delete()
   if (message.author.id !== '358771712839385089') return;
 client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
     message.channel.sendMessage(`**\`${argresult}\` :تم تغيير الحالة الى**`).then(msg => {msg.delete(7000)});
