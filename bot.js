7012cca469fdf4a45c6e9cf7c6eef131fa6da1ff
7MMo0oD2016 <moatseemibrahim@gmail.com>
2018-12-07 13:50:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,19 @@ client.user.setGame(argresult, "http://twitch.tv/y04zgamer");
 
  });
 
+client.on('message', msg => {
 
+    if (msg.content == '$join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("518513612139134986").join(); 
+    });
 
 client.login(process.env.BOT_TOKEN);
